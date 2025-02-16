0edf2b0df0bf0244b3d53844e04395d2aafc16e8
Asir Intisar <aintisar48@gmail.com>
2025-02-16 22:42:29 +0600

    [Create] - UserRegisterSerializer

--- account/serializers.py
+++ account/serializers.py
@@ -5,11 +5,36 @@ User = get_user_model()
 
 class UserRegisterSerializer(serializers.ModelSerializer):
     confirm_password = serializers.CharField(write_only=True)
-    
+        
     class Meta:
         model= User
         fields = ['first_name','last_name','username','email','password','confirm_password']
     
+    def save(self, **kwargs):
+        username = self.validate['username']
+        first_name = self.validate['first_name']
+        last_name = self.validate['last_name']
+        email = self.validate['email']
+        password = self.validate['password']
+        confirm_password = self.validate['confirm_password']
+        
+        if password != confirm_password:
+            raise serializers.ValidationError({'error':"Password don't match."})
+        
+        if User.objects.filter(email=email).exist():
+            raise serializers.ValidationError({'email':"Email already exist."})
+        
+        if User.objects.filter(username=username).exists():
+            raise serializers.ValidationError({'username':"Username already exist."})
+        
+        user = User(username=username,first_name=first_name,last_name=last_name,email=email)
+        user.set_password('password')
+        user.is_active = False
+        user.save()
+        
+        return user
+    
+        
     
         
     
