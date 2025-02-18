25fd7bd45cd6595f40d1eda556fe025bd12bcd6f
Asir Intisar <aintisar48@gmail.com>
2025-02-18 23:33:44 +0600

    [Create] - UserLoginAPIView

--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from rest_framework.views import APIView
-from .serializers import UserRegisterSerializer
-from rest_framework_simplejwt.tokens import AccessToken
+from .serializers import UserRegisterSerializer,UserLoginSerializer
+from rest_framework_simplejwt.tokens import RefreshToke,AccessToken
 from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
 from django.utils.encoding import force_bytes
 from rest_framework.response import Response
@@ -9,7 +9,9 @@ from rest_framework import status
 # Mail
 from django.template.loader import render_to_string
 from django.core.mail import EmailMultiAlternatives
+from django.contrib.auth import get_user_model,authenticate,login,logout
 
+User = get_user_model()
 
 # Create your views here.
 class UserRegisterAPIView(APIView):
@@ -44,3 +46,77 @@ class UserRegisterAPIView(APIView):
                 "message":"Registation failed. Please check the provided data.",
                 "error": serializer.errors,
             },status=status.HTTP_400_BAD_REQUEST)
+
+def activate(self,uid64,token):
+    try:
+        uid = urlsafe_base64_decode(uid64).decode()
+        # user = User.__default_manager.get(pk=uid)
+        user = User.objects.get(pk=uid)
+    except(User.DoesNotExist):
+        user = None
+        return redirect("http://127.0.0.1:8000/account/register/")
+    
+    try:
+        if user is not None and not user.is_active:
+            user.is_active = True
+            user.save()
+            return redirect('http://127.0.0.1:8000/account/login/')
+        else:
+            return redirect('http://127.0.0.1:8000/account/login/')
+    except Exception:
+        return redirect('http://127.0.0.1:8000/account/register/')
+    
+class UserLoginAPIView(APIView):
+    serializer_class = UserLoginSerializer
+    
+    def post(self,request):
+        serializer = self.get_serializer(data=request.data)
+        
+        if serializer.is_valid():
+            username_or_email = serializer.validated_data['username']
+            password = serializer.validated_data['password']
+            
+            user = User.objects.filter(username=username_or_email).first() or User.objects.filter(email=username_or_email).first()
+            if user:
+                user = authenticate(username=user.username,password=password)
+                if user:
+                    refresh = RefreshToke.for_user(user)
+                    login(request,user)
+                    
+                    return Response({
+                        "success":True,
+                        "statusCode":status.HTTP_200_OK,
+                        "message":"Looged in successfully",
+                        "data":{
+                            "refresh":str(refresh),
+                            "access": str(refresh.access_token),
+                            "user_id":user.id,
+                            "username":user.username,
+                        }
+                    },status=status.HTTP_200_OK)
+                else:
+                    return Response({
+                    "success":False,
+                    "message":"Invalid credentials.",
+                    "error":"Please check your username or password.",
+                    "statusCode":status.HTTP_400_BAD_REQUEST,
+                },status=status.HTTP_400_BAD_REQUEST)
+            else:
+               return Response({
+                "success":False,
+                "message":"Invalid username or email.",
+                "error":"User not found.",
+                "statusCode":status.HTTP_400_BAD_REQUEST
+            },status=status.HTTP_400_BAD_REQUEST) 
+        else:
+            return Response({
+                "success":False,
+                "message":"Invalid input",
+                "error": serializer.errors,
+                "statusCode":status.HTTP_400_BAD_REQUEST
+            },status=status.HTTP_400_BAD_REQUEST)
+            
+                
+        
+        
+        
